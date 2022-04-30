800b54545e7822c5575a17f48fefcc1f386f5942
Raffy E <raffy27@pm.me>
2022-04-30 16:44:11 +0300

    Untested check function

--- MouseDecoder.srcs/sources_1/new/MouseTypes.vhd
+++ MouseDecoder.srcs/sources_1/new/MouseTypes.vhd
@@ -36,38 +36,59 @@ package Mouse_Types is
         Z: STD_LOGIC_VECTOR(3 downto 0);
     end record;
     
-    function ParseMouseData(signal Buf: STD_LOGIC_VECTOR(0 to 42)) return Mouse_Message;
-    function IsMouseDataValid(signal Buf: STD_LOGIC_VECTOR(0 to 42)) return Boolean;
+    function ParseMouseData(signal Buf: STD_LOGIC_VECTOR(42 downto 0)) return Mouse_Message;
+    function IsMouseDataValid(signal Buf: STD_LOGIC_VECTOR(42 downto 0)) return Boolean;
     
 end package;
 
 package body Mouse_Types is
-    function ParseMouseData(signal Buf: STD_LOGIC_VECTOR(0 to 42)) return Mouse_Message is
+    function ParseMouseData(signal Buf: STD_LOGIC_VECTOR(42 downto 0)) return Mouse_Message is
     variable Msg: Mouse_Message;
     begin
         Msg.LeftClick   := Buf(42);
         Msg.RightClick  := Buf(41);
         Msg.MiddleClick := Buf(40);
-        -- Bit #3 == #39 is always 1
+        -- Bit #3 <==> #39 is always 1
         Msg.X(8) := Buf(38);
         Msg.Y(8) := Buf(37);
         Msg.OverflowX   := Buf(36);
         Msg.OverflowY   := Buf(35);
         -- Parity, Stop, Start
         
-        Msg.X(7 downto 0) := Buf(11 to 18);
+        Msg.X(7 downto 0) := Buf(31 downto 24);
         -- Parity, Stop, Start
-        Msg.Y(7 downto 0) := Buf(22 to 29);
+        Msg.Y(7 downto 0) := Buf(20 to 13);
         -- Parity, Stop, Start, Sign Extension or Secondary Buttons
-        Msg.Z := Buf(37 to 40);
+        Msg.Z := Buf(5 to 2);
         -- Stop bit
         
         return Msg;
     end function;
     
-    function IsMouseDataValid(signal Buf: STD_LOGIC_VECTOR(0 to 42)) return Boolean is
+    function ParityOf(signal Buf: STD_LOGIC_VECTOR) return STD_LOGIC is
+    variable P: STD_LOGIC := '0';
     begin
-        -- TODO
+        for I in Buf'RANGE loop
+            P := P xor Buf(I);
+        end loop;
+        return P;
+    end function;
+    
+    function IsMouseDataValid(signal Buf: STD_LOGIC_VECTOR(42 downto 0)) return Boolean is
+    begin
+        if Buf(39) /= '1' then return false; end if;
+        if ParityOf(Buf(42 downto 35)) /= Buf(34) then return false; end if;
+        if Buf(33) /= '1' or Buf(32) /= '0' then return false; end if;
+        
+        if ParityOf(Buf(31 downto 24)) /= Buf(23) then return false; end if;
+        if Buf(22) /= '1' or Buf(21) /= '0' then return false; end if;
+        
+        if ParityOf(Buf(20 downto 13)) /= Buf(12) then return false; end if;
+        if Buf(11) /= '1' or Buf(10) /= '0' then return false; end if;
+        
+        if ParityOf(Buf(9 downto 2)) /= Buf(1) then return false; end if;
+        if Buf(0) /= '1' then return false; end if;
+        
         return true;
     end function;
 end;
